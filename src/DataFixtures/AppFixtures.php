e29e7201ca7a22f541b0bfd4bbff3e165a80f89e
oliviercaccia <olivier.caccia.pro@gmail.com>
2021-06-08 08:57:07 +0200

    closes #11; Login/Logout feature

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,9 +7,22 @@ use App\Entity\Post;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    private function encode($user, $plaintextpassword)
+    {
+        return $this->passwordEncoder->encodePassword(
+            $user,
+            $plaintextpassword
+        );
+    }
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
@@ -24,7 +37,7 @@ class AppFixtures extends Fixture
         for ($i = 0; $i < 10; $i++) {
             $author = new User();
             $author->setNickname($faker->username);
-            $author->setPassword($faker->password);
+            $author->setPassword($this->encode($author, "mdp"));
             $author->setRoles(["USER_ROLE"]);
             $manager->persist($author);
         }
